2922f14c75034bbb66be4912656da0f59e17dcd2
YJH <altos0120@hanmail.net>
2018-05-17 01:54:12 +0900

    Fragment

---
+++ app/src/main/java/com/example/yjh/areyouready/Check.java
@@ -0,0 +1,23 @@
+package com.example.yjh.areyouready;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by YJH on 2018-05-17.
+ */
+
+public class Requirement extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.requirement, null);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/yjh/areyouready/Requirement.java
@@ -0,0 +1,8 @@
+package com.example.yjh.areyouready;
+
+/**
+ * Created by YJH on 2018-05-17.
+ */
+
+public class Requirement {
+}
