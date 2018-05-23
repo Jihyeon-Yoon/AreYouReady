2fa90254807ebe82970a87068def9ed1a35e342b
YJH <altos0120@hanmail.net>
2018-05-24 00:36:19 +0900

    Credit Part

--- app/src/main/java/com/example/yjh/areyouready/Credit.java
+++ app/src/main/java/com/example/yjh/areyouready/Credit.java
@@ -1,5 +1,7 @@
 package com.example.yjh.areyouready;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
@@ -10,18 +12,39 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
- * Created by YJH on 2018-05-17.
+ * Created by YJH on 2018-05-19.
  */
 
 public class Credit extends Fragment {
+    private TextView enterYear_textView;
+    private String[] items = {"2017", "2016", "2015", "2014", "2013"};
+
+    //spinner
+    Spinner spinner;
+    ArrayAdapter<String> adapterSpinner;
+
+    //shared preference
+    SharedPreferences sh_Pref;
+    SharedPreferences.Editor toEdit;
+    String enterYearTemp, enterYearFix;
+    Context context;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.credit, null);
 
+        //viewPager
         TabLayout tabLayout = rootView.findViewById(R.id.credit_tab);
         ViewPager viewPager = rootView.findViewById(R.id.credit_viewPager);
 
@@ -35,8 +58,55 @@ public class Credit extends Fragment {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        enterYear_textView = rootView.findViewById(R.id.enterYear_textView);
+
+        //spinner
+        spinner = (Spinner)rootView.findViewById(R.id.enterYear_spinner);
+        adapterSpinner = new ArrayAdapter<> (
+                getActivity(), android.R.layout.simple_spinner_item, items);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterSpinner);
+
+        //get stored enter year
+        applySharedPreference();
+
+        //set spinner default
+        int position = (2017 - (Integer.parseInt(enterYearFix)));
+        spinner.setSelection(position);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView adapterView, View view, int position, long id) {
+                    enterYear_textView.setText(items[position]);
+                    enterYearTemp = items[position];
+                    sharedPreferences();
+            }
+            @Override
+            public void onNothingSelected(AdapterView adapterView) {
+                    enterYear_textView.setText("2017");
+            }
+        });
+
+
         return rootView;
     }
+
+    public void sharedPreferences() {
+        context = getActivity();
+        sh_Pref = context.getSharedPreferences("STORE DATA", MODE_PRIVATE);
+        toEdit = sh_Pref.edit();
+        toEdit.putString("enterYearTemp", enterYearTemp);
+        toEdit.commit();
+    }
+
+    public void applySharedPreference() {
+        context = getActivity();
+        sh_Pref = context.getSharedPreferences("STORE DATA", MODE_PRIVATE);
+        if(sh_Pref != null && sh_Pref.contains("enterYearFix")) {
+            enterYearFix = sh_Pref.getString("enterYearFix", "2017");
+        }
+    }
+
 
     private class MyPagerAdapter extends FragmentPagerAdapter {
 
--- app/src/main/java/com/example/yjh/areyouready/CreditFirstFragment.java
+++ app/src/main/java/com/example/yjh/areyouready/CreditFirstFragment.java
@@ -1,11 +1,17 @@
 package com.example.yjh.areyouready;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -13,6 +19,20 @@ import android.view.ViewGroup;
  */
 public class CreditFirstFragment extends Fragment {
 
+    TableLayout table2017;
+    TableLayout table2016;
+    TableLayout table2015;
+    TableLayout table2014;
+    TableLayout table2013;
+
+
+    //shared preference
+    SharedPreferences sh_Pref;
+    SharedPreferences.Editor toEdit;
+    String enterYearTemp, enterYearFix;
+    int totalCredit;
+    int totalCreditNow;
+    Context context;
 
     public CreditFirstFragment() {
         // Required empty public constructor
@@ -22,8 +42,73 @@ public class CreditFirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_credit_first, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_credit_first, container, false);
+
+
+
+        table2017 = rootView.findViewById(R.id.tableLayout2017);
+        table2016 = rootView.findViewById(R.id.tableLayout2016);
+        table2015 = rootView.findViewById(R.id.tableLayout2015);
+        table2014 = rootView.findViewById(R.id.tableLayout2014);
+        table2013 = rootView.findViewById(R.id.tableLayout2013);
+
+        Button btn = rootView.findViewById(R.id.button2017);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                table2017.setVisibility(View.INVISIBLE);
+                table2016.setVisibility(View.VISIBLE);
+
+                applySharedPreference();
+                if(!enterYearFix.equals(enterYearTemp)) {
+                    totalCreditChoose();
+                    sharedPreferences();
+                }
+
+            }
+        });
+
+        return rootView;
+    }
+
+    private void totalCreditChoose() {
+        int temp = Integer.parseInt(enterYearTemp);
+        switch(temp) {
+            case 2017:
+            case 2016:
+                totalCredit = 121;
+                break;
+            case 2015:
+            case 2014:
+            case 2013:
+                totalCredit = 120;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void sharedPreferences() {
+        context = getActivity();
+        sh_Pref = context.getSharedPreferences("STORE DATA", MODE_PRIVATE);
+        toEdit = sh_Pref.edit();
+        toEdit.putString("enterYearFix", enterYearTemp);
+        toEdit.putInt("totalCredit", totalCredit);
+        toEdit.commit();
+    }
+
+    public void applySharedPreference() {
+        context = getActivity();
+        sh_Pref = context.getSharedPreferences("STORE DATA", MODE_PRIVATE);
+        if(sh_Pref != null && sh_Pref.contains("enterYearTemp")) {
+            enterYearTemp = sh_Pref.getString("enterYearTemp", "2013");
+        }
+        if(sh_Pref != null && sh_Pref.contains("enterYearFix")) {
+            enterYearFix = sh_Pref.getString("enterYearFix", "2017");
+        }
     }
 
 }
