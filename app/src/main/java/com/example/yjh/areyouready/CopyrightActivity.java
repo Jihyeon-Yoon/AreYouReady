950a02161bcc1f4043f98a980abeaaf03d30a378
yohan5050 <yohan5050@naver.com>
2018-06-08 02:26:25 +0900

    Add webview on copyright page

--- app/src/main/java/com/example/yjh/areyouready/CopyrightActivity.java
+++ app/src/main/java/com/example/yjh/areyouready/CopyrightActivity.java
@@ -2,6 +2,7 @@ package com.example.yjh.areyouready;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class CopyrightActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class CopyrightActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_copyright);
+
+            // 졸업요건 교양과목 표를 WebView를 이용해서 보여줄 것임.
+        WebView mWebView = findViewById(R.id.wv_liberal_arts);
+
+        // assets 폴더 안의 www 폴더 안에 html파일이 있음. 그 html 파일이 표 이미지를 보여주는 html 파일임.
+        mWebView.loadUrl("file:///android_asset/www/test.html");
     }
 }
